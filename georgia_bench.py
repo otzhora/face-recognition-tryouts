5b0b8df8965bf3066e7f35669201c75d264dddf8
Yuriy Rogachev <y_rogachev@inbox.ru>
2018-11-06 20:52:03 +0300

    yale and gd benchmarks

---
+++ georgia_bench.py
@@ -0,0 +1,43 @@
+import face_recognition
+import glob
+import time
+
+start_time = time.time()
+
+unknown_images_path = "./georgia_tech/unknown_persons/"
+known_images_path = "./georgia_tech/known_persons/"
+
+
+known_faces = {}
+for filename in glob.glob(known_images_path+"*.jpg"):
+    try:
+        name = filename[filename.rfind('/') + 1: filename.rfind('_')]
+        image = face_recognition.load_image_file(filename)
+        known_faces[name] = face_recognition.face_encodings(image)[0]
+
+    except IndexError:
+        print("something went wrong")
+
+correct = 0
+incorrect = 0
+for filename in glob.glob(unknown_images_path+"*.jpg"):
+    im = face_recognition.load_image_file(filename)
+    try:
+        face_encodings = face_recognition.face_encodings(im)
+    except IndexError:
+        print('no face found on photo {}'.format(filename))
+        continue
+
+    for name in known_faces:
+        res = face_recognition.compare_faces(face_encodings, known_faces[name])
+        if True in res:
+            ans_name = filename[filename.rfind('/') + 1: filename.rfind('_')]
+            if ans_name != name:
+                print("err. Found {} on {}".format(name, filename))
+                incorrect += 1
+            else:
+                correct += 1
+
+print(time.time() - start_time)
+print("corr: {}".format(correct))
+print("wrong: {}".format(incorrect))
